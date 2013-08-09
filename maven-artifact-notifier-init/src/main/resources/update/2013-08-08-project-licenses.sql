5fc51154c8122f06795a4026f1c3993011d989c0
Stephane Gleizes <stephane.gleizes@openwide.fr>
2013-08-09 17:15:01 +0200

    Add ProjectLicense SQL insert script

---
+++ maven-artifact-notifier-init/src/main/resources/update/2013-08-08-project-licenses.sql
@@ -0,0 +1,16 @@
+BEGIN;
+
+INSERT INTO projectlicense VALUES(1, 'f', 'f', 'f', 't', 'GNU General Public License 2.0', 0, 'GPL-2.0', 'http://opensource.org/licenses/GPL-2.0');
+INSERT INTO projectlicense VALUES(2, 'f', 'f', 'f', 't', 'GNU General Public License 3.0', 1, 'GPL-3.0', 'http://opensource.org/licenses/GPL-3.0');
+
+INSERT INTO projectlicense VALUES(3, 'f', 'f', 'f', 't', 'GNU Lesser General Public License 2.1', 2, 'LGPL-2.1', 'http://opensource.org/licenses/LGPL-2.1');
+INSERT INTO projectlicense VALUES(4, 'f', 'f', 'f', 't', 'GNU Lesser General Public License 3.0', 3, 'LGPL-3.0', 'http://opensource.org/licenses/LGPL-3.0');
+
+INSERT INTO projectlicense VALUES(5, 'f', 'f', 'f', 't', 'BSD 2-Clause License', 4, 'BSD-2', 'http://opensource.org/licenses/BSD-2-Clause');
+INSERT INTO projectlicense VALUES(6, 'f', 'f', 'f', 't', 'BSD 3-Clause License', 5, 'BSD-3', 'http://opensource.org/licenses/BSD-3-Clause');
+
+INSERT INTO projectlicense VALUES(7, 'f', 'f', 'f', 't', 'Apache License 2.0', 6, 'AL-2.0', 'http://opensource.org/licenses/Apache-2.0');
+INSERT INTO projectlicense VALUES(8, 'f', 'f', 'f', 't', 'Eclipse Public License', 7, 'EPL-1.0', 'http://opensource.org/licenses/EPL-1.0');
+INSERT INTO projectlicense VALUES(9, 'f', 'f', 'f', 't', 'MIT License', 8, 'MIT', 'http://opensource.org/licenses/MIT');
+
+COMMIT;
