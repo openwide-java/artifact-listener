6a009a6affecbfb02e8525c9714e70ea84b79f82
Guillaume Smet <guillaume.smet@gmail.com>
2014-11-03 18:06:14 +0100

    Provide a mock implementation for the notification service

--- maven-artifact-notifier-core/src/test/java/fr/openwide/maven/artifact/notifier/core/test/config/spring/MavenArtifactNotifierCoreTestCommonConfig.java
+++ maven-artifact-notifier-core/src/test/java/fr/openwide/maven/artifact/notifier/core/test/config/spring/MavenArtifactNotifierCoreTestCommonConfig.java
@@ -1,10 +1,13 @@
 package fr.openwide.maven.artifact.notifier.core.test.config.spring;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
-import fr.openwide.maven.artifact.notifier.core.config.spring.MavenArtifactNotifierCoreCommonConfig;
 import fr.openwide.core.spring.config.spring.annotation.ConfigurationLocations;
+import fr.openwide.maven.artifact.notifier.core.business.notification.service.EmptyNotificationContentDescriptorFactoryImpl;
+import fr.openwide.maven.artifact.notifier.core.business.notification.service.IArtifactNotifierNotificationContentDescriptorFactory;
+import fr.openwide.maven.artifact.notifier.core.config.spring.MavenArtifactNotifierCoreCommonConfig;
 
 @Configuration
 @ConfigurationLocations(locations = { "classpath:configuration-test.properties" }, order = 1000)
@@ -12,5 +15,10 @@ import fr.openwide.core.spring.config.spring.annotation.ConfigurationLocations;
 	MavenArtifactNotifierCoreCommonConfig.class,
 })
 public class MavenArtifactNotifierCoreTestCommonConfig {
+	
+	@Bean
+	public IArtifactNotifierNotificationContentDescriptorFactory<?> notificationContentDescriptorFactory() {
+		return new EmptyNotificationContentDescriptorFactoryImpl();
+	}
 
 }
